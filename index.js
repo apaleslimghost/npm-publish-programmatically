d64a1e0542d61c38ecedd98d392f4bdfcff4386d
Matt Brennan <matt@153.io>
2016-12-26 20:12:15 +0000

    the actual thing

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 const tgzFiles = require('@quarterto/tgz-files');
 const Registry = require('npm-registry-client');
+const promisify = require('@quarterto/promisify');
 
 const client = new Registry();
+client.publish = promisify(client.publish);
 
-const auth = {
-	token: process.env.NPM_TOKEN
-};
+module.exports = (files, {auth, registry = 'https://registry.npmjs.com', access = 'public'} = {}) => Promise.resolve().then(() => {
+	if(!files['package.json']) throw new Error('No package.json in files object');
 
-const id = Math.floor(parseInt('zzzzzz', 36) * Math.random()).toString(36);
-const name = `quarterto-test-${id}`;
+	const metadata = files['package.json'].name ? files['package.json'] : JSON.parse(files['package.json']);
+	const {name} = metadata;
+	const body = tgzFiles(files, {prefix: 'package'});
 
-const metadata = {
-	name,
-	version: '0.0.0',
-};
-
-const body = tgzFiles({
-	'package.json': JSON.stringify(metadata),
-	'index.js': `console.log(${JSON.stringify(name)})`
-}, {prefix: 'package'});
-
-client.publish(`https://registry.npmjs.com/${name}`, {
-	body,
-	auth,
-	access: 'public',
-	metadata,
-}, console.log);
+	return client.publish(`${registry}/${name}`, {metadata, access, auth, body});
+});
